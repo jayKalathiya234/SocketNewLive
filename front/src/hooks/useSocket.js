6e122dbc6593cafc25fe9b28a469363e84b65519
jay <jay.kalathiyainfotech123@gmail.com>
2025-04-18 16:39:25 +0530

    new

--- front/src/hooks/useSocket.js
+++ front/src/hooks/useSocket.js
@@ -24,7 +24,7 @@ const decryptMessage = (encryptedText) => {
   return result;
 };
 
-const SOCKET_SERVER_URL = "https://chat-message-0fml.onrender.com"; // Move to environment variable in production
+const SOCKET_SERVER_URL = "https://chat-message-0fml.onrender.com/"; // Move to environment variable in production
 
 export const useSocket = (userId, localVideoRef, remoteVideoRef, allUsers) => {
   const [isConnected, setIsConnected] = useState(false);
